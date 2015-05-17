3494237cb278b24f32293297ca072a919a6635e3
zrajm <zrajm@klingonska.org>
2015-05-18 01:51:56 +0200

    Now scales coordinates no normalized positions.

--- script.js
+++ script.js
@@ -47,24 +47,59 @@ jQuery.fn.hasAnyClass = function (selector) {
     //  Note Module
     //
     function makeNoteCache(opt) {
-        var self, noteCache = {}, session = random132BitString();
+        var self, noteCache = {}, session = random132BitString(),
+            boardPixelSize = {
+                x: opt.width  || 0,
+                y: opt.height || 0
+            };
         opt = {
             afterSet: opt.afterSet || function () { return; },
             delete  : opt.delete   || function () { return; },
             getAll  : opt.getAll   || function () { return; },
             poll    : opt.poll     || function () { return; },
-            push    : opt.push     || function () { return; }
+            push    : opt.push     || function () { return; },
+            redraw  : opt.redraw   || function () { return; }
         };
 
         function processPollResponse(noteUpdates) {
             $.each(noteUpdates, function (id, values) {
                 if (!values) {                 // no values = always delete
                     self.delete(id);
-                } else if (values.by !== session) {
-                    self.set(id, values);      // only set if from non-self
+                    return;
                 }
+                if (values.by === session) {   // values set by self = skip
+                    return;
+                }
+                self.set(id, values, false, true);
             });
         }
+
+        function percentify(value, maxValue) {
+            var percent = (value / maxValue) * 100;
+            return +(percent.toFixed(3));      // round off to 3 decimals
+        }
+        function pixelize(value, maxValue) {
+            var pixel = (value * maxValue) / 100;
+            return +(pixel.toFixed());         // round off to whole number
+        }
+
+        var jqProp = { x: "outerWidth", y: "outerHeight" }
+        function getSame(value, prop, id) {
+            var boardSize = boardPixelSize[prop],
+                // FIXME: Hardcoded note width/height ('150' pixel)
+                // (used only when GUI note does not exist yet, i.e. on init)
+                noteSize = $("#" + id)[ jqProp[prop] ]() || 150,
+                maxValue = boardSize - noteSize;
+            return pixelize(value, maxValue);
+        }
+        function setSame(value, prop, id) {
+            var boardSize  = boardPixelSize[prop],
+                noteSize = $("#" + id)[ jqProp[prop] ]() || 150,
+                maxValue   = boardSize - noteSize;
+            return percentify(value, maxValue);
+        }
+        var getConv = { x: getSame, y: getSame };
+        var setConv = { x: setSame, y: setSame };
 
         self = {
             getZMax: function () {
@@ -80,12 +115,35 @@ jQuery.fn.hasAnyClass = function (selector) {
                         JSON.stringify(noteCache, null, 4);
             },
             get: function (id, prop) {
-                return prop ? noteCache[id][prop] : noteCache[id];
+                var value, note = {};
+                if (!noteCache[id]) {          // non-existing = null
+                    return null;
+                }
+                if (prop) {                    // single property
+                    value = noteCache[id][prop];
+                    return getConv[prop] ?
+                        getConv[prop](value, prop, id) :
+                        value;
+                }                              // whole note
+                $.each(noteCache[id], function(prop, value) {
+                    note[prop] = getConv[prop] ?
+                        getConv[prop](value, prop, id) :
+                        value;
+                });
+                return note;
             },
-            set: function (id, values, suppressNoteUpdate) {
+            set: function (id, values, suppressNoteUpdate, suppressConv) {
+                // suppressNoteUpdate -- suppresses note update in GUI
+                // suppressConv -- suppresses normalization of note positions
                 noteCache[id] = noteCache[id] || {};
                 $.each(values, function (prop, value) {
-                    noteCache[id][prop] = value;
+                    if (suppressConv) {
+                        noteCache[id][prop] = value;
+                    } else {
+                        noteCache[id][prop] = setConv[prop] ?
+                            setConv[prop](value, prop, id) :
+                            value;
+                    }
                 });
                 opt.afterSet(id, suppressNoteUpdate);
                 return this;
@@ -108,12 +166,21 @@ jQuery.fn.hasAnyClass = function (selector) {
                     callback(serverResponse);
                 });
                 return this;
+            },
+            setSize: function (width, height) {
+                if (boardPixelSize.x === width && boardPixelSize.y === height
+                ) { return this; }
+                boardPixelSize = { x: width, y: height };
+                $.each(noteCache, function (id, values) {
+                    opt.redraw(id, values);
+                });
+                return this;
             }
         };
         self.pullAll(function () {
             opt.getAll(function (serverResponse) {
                 $.each(serverResponse, function (id, values) {
-                    self.set(id, values);
+                    self.set(id, values, false, true);
                 });
                 // Initiate long polling.
                 opt.poll(processPollResponse, opt.poll, session);
@@ -175,7 +242,6 @@ jQuery.fn.hasAnyClass = function (selector) {
                         selected.setColorClass(color);
                         notes.push(id, { color: color });
                     } else {                   // create new note
-                        // Refactor: Check that id does not already exist.
                         var id = random132BitString();
                         notes.push(id, {
                             // Refactor: Width/height should come from CSS rule.
@@ -305,7 +371,16 @@ jQuery.fn.hasAnyClass = function (selector) {
         push: function (id, json) {
             request({ type: "PUT", url: "api/put.cgi?" + id, data: json }).
                 done(hideError);
+        },
+        redraw: function (id, note) {
+            /*jslint unparam:true */
+            drawNote(id, notes);
         }
+    }).setSize(mainElement.width(), mainElement.height());
+
+    // Recalculate board size & redraw notes on window size change.
+    $(window).resize(function () {
+        notes.setSize(mainElement.width(), mainElement.height());
     });
 
     mainElement.on("input", function (event) {
